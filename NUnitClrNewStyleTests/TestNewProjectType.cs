28c8a2cfafcc1834cf6ae385a4029d6a4e17dce6
Rob Prouse <rob@prouse.org>
2017-04-17 21:50:36 -0400

    Adding addtional test projects

---
+++ NUnitClrNewStyleTests/TestNewProjectType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnitClrNewStyleTests
+{
+    [TestFixture]
+    public class TestNewProjectType
+    {
+        [Test]
+        public void NewStyleUnitTests()
+        {
+            Assert.Pass("This works too!");
+        }
+    }
+}
