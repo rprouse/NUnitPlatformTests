0c9a01b5afc4847e312df18f9b55c4dc3cb2e5de
Rob Prouse <rob@prouse.org>
2017-04-25 22:00:27 -0400

    Added more async tests

--- src/NetStandard16Library/AsyncMethods.cs
+++ src/NetStandard16Library/AsyncMethods.cs
@@ -7,5 +7,19 @@ namespace NetStandard16Library
     {
         public async Task<bool> IsTrueAsync() =>
             await Task.FromResult(true);
+
+        public async Task<int> AddAfterDelay(int x, int y)
+        {
+            if (x == int.MaxValue || x == int.MinValue)
+                throw new ArgumentException("Cannot be MinValue or MaxValue", nameof(x));
+
+            await Task.Delay(10);
+
+            if (y == int.MaxValue || y == int.MinValue)
+                throw new ArgumentException("Cannot be MinValue or MaxValue", nameof(y));
+
+
+            return x + y;
+        }
     }
 }
--- test/NetCore11Tests/AsyncTests.cs
+++ test/NetCore11Tests/AsyncTests.cs
@@ -24,5 +24,38 @@ namespace NetCore11Tests
             var result = await _methods.IsTrueAsync();
             Assert.That(result, Is.True);
         }
+
+        [TestCase(1, 1, 2)]
+        [TestCase(1, 10, 11)]
+        [TestCase(42, 42, 84)]
+        [TestCase(100, 12, 112)]
+        [TestCase(-1, -1, -2)]
+        [TestCase(1, -1, 0)]
+        [TestCase(-100, 1, -99)]
+        [TestCase(6, 4, 10)]
+        [TestCase(10, -15, -5)]
+        public async Task CanAddAfterDelay(int x, int y, int expected)
+        {
+            var result = await _methods.AddAfterDelay(x, y);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(int.MaxValue, 1)]
+        [TestCase(int.MinValue, 1)]
+        [TestCase(1, int.MaxValue)]
+        [TestCase(1, int.MinValue)]
+        public void ThrowsArgumentException(int x, int y)
+        {
+            Assert.That(async () => await _methods.AddAfterDelay(x, y), Throws.ArgumentException);
+        }
+
+        [TestCase(int.MaxValue, 1)]
+        [TestCase(int.MinValue, 1)]
+        [TestCase(1, int.MaxValue)]
+        [TestCase(1, int.MinValue)]
+        public void ThrowsArgumentExceptionClassicStyle(int x, int y)
+        {
+            Assert.ThrowsAsync<ArgumentException>(async () => await _methods.AddAfterDelay(x, y));
+        }
     }
 }
