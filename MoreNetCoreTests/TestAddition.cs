3440097c21c7269f4a5dde29e121fdfb03c30738
Rob Prouse <rob@prouse.org>
2017-04-25 21:29:21 -0400

    Added a .NET Standard 1.3 project

---
+++ MoreNetCoreTests/TestAddition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using NetStandard13Library;
+using NUnit.Framework;
+
+namespace MoreNetCoreTests
+{
+    [TestFixture]
+    public class TestAddition
+    {
+        Addition _add;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _add = new Addition();
+        }
+
+        [TestCaseSource(nameof(AdditionData))]
+        public void CanAddTwoNumbers(int x, int y, int expected)
+        {
+            var result = _add.Add(x, y);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        public static IEnumerable AdditionData()
+        {
+            yield return new TestCaseData(1, 1, 2);
+            yield return new TestCaseData(1, 10, 11);
+            yield return new TestCaseData(42, 42, 84);
+            yield return new TestCaseData(100, 12, 112);
+            yield return new TestCaseData(-1, -1, -2);
+            yield return new TestCaseData(1, -1, 0);
+            yield return new TestCaseData(-100, 1, -99);
+            yield return new TestCaseData(6, 4, 10);
+            yield return new TestCaseData(10, -15, -5);
+        }
+    }
+}
---
+++ NetStandard13Library/Addition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NetStandard13Library
+{
+    public class Addition
+    {
+        public int Add(int x, int y)
+        {
+            if (x == int.MaxValue || x == int.MinValue)
+                throw new ArgumentException("Cannot be MinValue or MaxValue", nameof(x));
+
+            if (y == int.MaxValue || y == int.MinValue)
+                throw new ArgumentException("Cannot be MinValue or MaxValue", nameof(y));
+
+            return x + y;
+        }
+    }
+}
