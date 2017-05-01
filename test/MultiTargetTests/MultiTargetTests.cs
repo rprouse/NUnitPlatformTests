1968c0ba1b265201c95636dcca34298e1bd07a83
Rob Prouse <rob@prouse.org>
2017-05-01 18:24:52 -0400

    Added multiple platform tests

---
+++ test/MultiTargetTests/MultiTargetTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace MultiTargetTests
+{
+    [TestFixture]
+    public class MultiTargetTests
+    {
+        [Test]
+        public void TestOnMultiplePlatforms()
+        {
+#if NETCOREAPP1_1
+            Assert.Pass(".NET Core");
+#else
+            Assert.Pass(".NET Desktop");
+#endif
+        }
+    }
+}
