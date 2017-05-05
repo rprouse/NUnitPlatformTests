21a527e74965d80bac5ffcaba2a63601a68e32f6
Rob Prouse <rob@prouse.org>
2017-05-04 22:52:14 -0400

    Testing .NET 3.5

---
+++ Net35Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Net35Tests
+{
+    [TestFixture]
+    public class Net35Tests
+    {
+        [Test]
+        public void TestNet35Method()
+        {
+            Assert.Pass("Pass for the win!");
+        }
+    }
+}
