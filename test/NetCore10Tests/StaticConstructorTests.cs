ef6b8103d6c46200d152539d308096792b39b2eb
Rob Prouse <rob@prouse.org>
2017-05-30 08:42:43 -0400

    Testing static constructor that throws

---
+++ test/NetCore10Tests/StaticConstructorTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace NetCore10Tests
+{
+    [TestFixture]
+    public class StaticConstructorTests
+    {
+        static class ClassWithStaticConstructor
+        {
+            static ClassWithStaticConstructor()
+            {
+                throw new Exception("Crash!");
+            }
+
+            public static int Add(int x, int y) => x + y;
+        }
+
+        [Test]
+        public void UseClassWithStaticConstructor()
+        {
+            Assert.That(ClassWithStaticConstructor.Add(2, 2), Is.EqualTo(4));
+        }
+    }
+}
