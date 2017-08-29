7b986364ed7aef609b8206fa8bcb3e3580196ff4
Rob Prouse <rob.prouse@bentley.com>
2017-08-28 21:55:34 -0400

    Add a .NET Standard/Core 2.0 library and test suite

---
+++ src/NetStandard20Library/Increment.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NetStandard20Library
+{
+    public static class Increment
+    {
+        public static int AddOne(this int x) => ++x;
+    }
+}
---
+++ test/NetCore20Tests/TestIncrement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NetStandard20Library;
+using NUnit.Framework;
+
+namespace NetCore20Tests
+{
+    [TestFixture]
+    public class TestIncrement
+    {
+        [Test]
+        public void IncrementAddsOne()
+        {
+            Assert.That(1.AddOne(), Is.EqualTo(2));
+        }
+    }
+}
